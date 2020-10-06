839d04a841207714b3f8dd2f6b493fb3d9992368
Luiz Paulo <lpfree76@hotmail.com>
2020-10-06 18:55:07 -0300

    Database Seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into tb_category (name) values ('electronics');
+insert into tb_category (name) values ('books');
+insert into tb_category (name) values ('cds');
+insert into tb_category (name) values ('dvds');
+insert into tb_category (name) values ('newspapers');
+insert into tb_category (name) values ('stationery');
